4bfc4ad9383e44db517ffa7d14ad1bef87dad446
PedroHenrique-ueg <84983523+PedroHenrique-ueg@users.noreply.github.com>
2021-07-26 23:27:32 -0300

    funcionario

---
+++ src/app/modelos/estabelecimentoModel.ts
@@ -0,0 +1,9 @@
+
+   export class Estabelecimento{
+    public id: any;
+    public nome: any;
+    public totalVagas: any;
+    public totalVagaCarro: any;
+    public totalVagaMoto: any;
+    public idEmpresa: any;
+ }
---
+++ src/app/modelos/funcionarioModel.ts
@@ -0,0 +1,10 @@
+export class funcionario{
+    public id: any;
+	public nome: any;
+	public cpf: any;
+	public dataNascimento: any;
+	public endereco: any;
+	public telefone: any;
+	public email: any;
+	public senha: any;
+}
